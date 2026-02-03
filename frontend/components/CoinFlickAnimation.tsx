3b40863cd65673f32d8f7ecef54c043d73089a71
lim <maesterong@limui-MacBookPro.local>
2026-02-03 20:59:23 +0900

    Add pixel art hand flick GIF animations

--- frontend/components/CoinFlickAnimation.tsx
+++ frontend/components/CoinFlickAnimation.tsx
@@ -5,23 +5,23 @@ import Image from 'next/image'
 export default function CoinFlickAnimation() {
   return (
     <>
-      {/* 왼쪽 엄지손가락 튕기기 */}
-      <div className="fixed left-4 md:left-10 bottom-[15%] md:bottom-[20%] pointer-events-none z-10">
-        <div className="relative w-32 h-48">
-          {/* 동전 1 - FUNS 글자 */}
-          <div className="absolute left-1/2 -translate-x-1/2 bottom-20 animate-flick-coin-shot">
+      {/* 왼쪽 손가락 튕기기 */}
+      <div className="fixed left-2 md:left-6 bottom-[12%] md:bottom-[18%] pointer-events-none z-10">
+        <div className="relative w-40 h-52">
+          {/* 동전 1 */}
+          <div className="absolute left-1/2 -translate-x-1/2 bottom-32 animate-flick-coin-shot">
             <Image
               src="/FUNS_back_logo.png"
               alt="FUNS"
               width={80}
               height={80}
-              className="w-16 h-16 md:w-20 md:h-20 object-contain"
+              className="w-14 h-14 md:w-18 md:h-18 object-contain"
             />
           </div>
 
-          {/* 동전 2 - FUNS 스마일 */}
+          {/* 동전 2 */}
           <div
-            className="absolute left-1/2 -translate-x-1/2 bottom-20 animate-flick-coin-shot"
+            className="absolute left-1/2 -translate-x-1/2 bottom-32 animate-flick-coin-shot"
             style={{ animationDelay: '0.4s' }}
           >
             <Image
@@ -29,13 +29,13 @@ export default function CoinFlickAnimation() {
               alt="FUNS"
               width={80}
               height={80}
-              className="w-14 h-14 md:w-16 md:h-16 object-contain"
+              className="w-12 h-12 md:w-16 md:h-16 object-contain"
             />
           </div>
 
-          {/* 동전 3 - FUNS 글자 */}
+          {/* 동전 3 */}
           <div
-            className="absolute left-1/2 -translate-x-1/2 bottom-20 animate-flick-coin-shot"
+            className="absolute left-1/2 -translate-x-1/2 bottom-32 animate-flick-coin-shot"
             style={{ animationDelay: '0.8s' }}
           >
             <Image
@@ -43,37 +43,38 @@ export default function CoinFlickAnimation() {
               alt="FUNS"
               width={80}
               height={80}
-              className="w-12 h-12 md:w-14 md:h-14 object-contain"
+              className="w-10 h-10 md:w-14 md:h-14 object-contain"
             />
           </div>
 
-          {/* 엄지손가락 튕기기 - 왼쪽 (오른쪽 방향) */}
-          <div
-            className="absolute bottom-0 left-1/2 -translate-x-1/2 text-7xl md:text-8xl animate-flick-finger-up"
-            style={{ transformOrigin: 'bottom center' }}
-          >
-            👍
+          {/* 왼쪽 손 GIF (엄지 튕기기) */}
+          <div className="absolute bottom-0 left-0">
+            <img
+              src="/hand_flick_left.gif"
+              alt="hand flick"
+              className="w-36 h-36 md:w-44 md:h-44 object-contain"
+            />
           </div>
         </div>
       </div>
 
-      {/* 오른쪽 엄지손가락 튕기기 */}
-      <div className="fixed right-4 md:right-10 bottom-[15%] md:bottom-[20%] pointer-events-none z-10">
-        <div className="relative w-32 h-48">
-          {/* 동전 1 - FUNS 스마일 */}
-          <div className="absolute left-1/2 -translate-x-1/2 bottom-20 animate-flick-coin-shot">
+      {/* 오른쪽 손가락 튕기기 */}
+      <div className="fixed right-2 md:right-6 bottom-[12%] md:bottom-[18%] pointer-events-none z-10">
+        <div className="relative w-40 h-52">
+          {/* 동전 1 */}
+          <div className="absolute left-1/2 -translate-x-1/2 bottom-32 animate-flick-coin-shot">
             <Image
               src="/FUNS_front_logo.png"
               alt="FUNS"
               width={80}
               height={80}
-              className="w-16 h-16 md:w-20 md:h-20 object-contain"
+              className="w-14 h-14 md:w-18 md:h-18 object-contain"
             />
           </div>
 
-          {/* 동전 2 - FUNS 글자 */}
+          {/* 동전 2 */}
           <div
-            className="absolute left-1/2 -translate-x-1/2 bottom-20 animate-flick-coin-shot"
+            className="absolute left-1/2 -translate-x-1/2 bottom-32 animate-flick-coin-shot"
             style={{ animationDelay: '0.4s' }}
           >
             <Image
@@ -81,13 +82,13 @@ export default function CoinFlickAnimation() {
               alt="FUNS"
               width={80}
               height={80}
-              className="w-14 h-14 md:w-16 md:h-16 object-contain"
+              className="w-12 h-12 md:w-16 md:h-16 object-contain"
             />
           </div>
 
-          {/* 동전 3 - FUNS 스마일 */}
+          {/* 동전 3 */}
           <div
-            className="absolute left-1/2 -translate-x-1/2 bottom-20 animate-flick-coin-shot"
+            className="absolute left-1/2 -translate-x-1/2 bottom-32 animate-flick-coin-shot"
             style={{ animationDelay: '0.8s' }}
           >
             <Image
@@ -95,16 +96,17 @@ export default function CoinFlickAnimation() {
               alt="FUNS"
               width={80}
               height={80}
-              className="w-12 h-12 md:w-14 md:h-14 object-contain"
+              className="w-10 h-10 md:w-14 md:h-14 object-contain"
             />
           </div>
 
-          {/* 엄지손가락 튕기기 - 오른쪽 (왼쪽 방향, 좌우 반전) */}
-          <div
-            className="absolute bottom-0 left-1/2 -translate-x-1/2 text-7xl md:text-8xl animate-flick-finger-up scale-x-[-1]"
-            style={{ transformOrigin: 'bottom center' }}
-          >
-            👍
+          {/* 오른쪽 손 GIF (좌우반전 버전) */}
+          <div className="absolute bottom-0 right-0">
+            <img
+              src="/hand_flick_right.gif"
+              alt="hand flick"
+              className="w-36 h-36 md:w-44 md:h-44 object-contain"
+            />
           </div>
         </div>
       </div>
